689f49132e256f7a3922e6a7a7d4cf0734dfd266
Julián Cano <juliancano.m96@gmail.com>
2022-05-10 16:21:34 -0300

    TG: filter task by partial name

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 // use "import" to import libraries
-const express = require('express');
+import express from 'express';
+
+const tasks = require('./resources/tasks');
 
 // use "require" to import JSON files
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/tasks', require('./resources/tasks'));
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
+
+// tasks router
+app.get('/tasks', tasks.getAll);
+app.get('/tasks/getByFilter', tasks.getByFilter);
+app.get('/tasks/:id', tasks.getById);
+app.put('/tasks/:id', tasks.updateById);
+app.delete('/tasks/:id', tasks.deleteById);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -1,33 +1,75 @@
-// use "import" to import libraries
-const express = require('express');
-
-const routerTasks = express.Router();
-
-// use "require" to import JSON files
-
+/* eslint-disable max-len */
+/* eslint-disable no-param-reassign */
+/* const fs = require('fs'); */
 const tasks = require('../data/tasks.json');
 
 // Gets all tasks
-routerTasks.get('/', (req, res) => {
+const getAll = (req, res) => {
   res.status(200).json({
     data: tasks,
   });
-});
+};
 
 // Get single task
-routerTasks.get('/:id', (req, res) => {
+const getById = (req, res) => {
   const found = tasks.some((task) => task.id === parseInt(req.params.id, 10));
   if (found) {
-    res.json(tasks.filter((task) => task.id === parseInt(req.params.id, 10)));
+    res.json(tasks.find((task) => task.id === parseInt(req.params.id, 10)));
   } else {
     res.status(400).json({ msg: `No task found with the id of ${req.params.id}` });
   }
-});
+};
+
+// Filter by partial name value
+const getByFilter = (req, res) => {
+  const query = req.query.name;
+  const tasksFiltered = tasks.filter((val) => val.name.toLowerCase().indexOf(query.toLowerCase()) > -1);
+  if (tasksFiltered.length > 0) {
+    res.send(tasksFiltered);
+  } else {
+    res.send(`There are no tasks matching the filter: "${query}"`);
+  }
+};
 
 // Create a task
-/* routerTasks.post('/', (req, res) => {
+/* const create = (req, res) => {
   const newTask = {
 
   };
-}); */
-module.exports = routerTasks;
+}; */
+
+// Edit a task
+const updateById = (req, res) => {
+  const found = tasks.some((task) => task.id === parseInt(req.params.id, 10));
+  if (found) {
+    const editTask = req.body;
+    tasks.forEach((task) => {
+      if (task.id === parseInt(req.params.id, 10)) {
+        task.name = editTask.name ? editTask.name : task.name;
+        task.details = editTask.details ? editTask.details : task.details;
+        res.json({ msg: 'Task updated', task });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No task found with the id of ${req.params.id}` });
+  }
+};
+
+// Delete task
+const deleteById = (req, res) => {
+  const found = tasks.some((task) => task.id === parseInt(req.params.id, 10));
+  if (found) {
+    res.json({ msg: 'Task deleted', tasks: tasks.filter((task) => task.id !== parseInt(req.params.id, 10)) });
+  } else {
+    res.status(400).json({ msg: `No task found with the id of ${req.params.id}` });
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+  updateById,
+  deleteById,
+  getByFilter,
+/*   create, */
+};
