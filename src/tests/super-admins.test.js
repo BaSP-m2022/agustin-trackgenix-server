5fa4fc618fde9de216f63360015e4f7f1612623a
Corina Arreguez <coriarreguez@gmail.com>
2022-05-23 19:22:42 -0300

    TG-12: get by id test

--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -43,3 +43,55 @@ describe('Get the super admin list', () => {
     expect(response.data).toBeUndefined();
   });
 });
+
+describe('Get super admin by id', () => {
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).get('/api/super-admins/6283f334fcd4fe8e83f960d4').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return a false error', async () => {
+    const response = await request(app).get('/api/super-admins/6283f334fcd4fe8e83f960d4').send();
+    expect(response.error).toBeFalsy();
+  });
+
+  test('Response should return a super admin', async () => {
+    const response = await request(app).get('/api/super-admins/6283f334fcd4fe8e83f960d4').send();
+    expect(response.body.data).not.toBeNull();
+  });
+
+  test('Response should return a message', async () => {
+    const response = await request(app).get('/api/super-admins/6283f334fcd4fe8e83f960d4').send();
+    expect(response.message).not.toBeNull();
+  });
+
+  test('Response should return 400 status', async () => {
+    const response = await request(app).get('/api/super-admins/:id').send();
+    expect(response.status).toBe(400);
+  });
+
+  test('Response should return a true error', async () => {
+    const response = await request(app).get('/api/super-admins/:id').send();
+    expect(response.error).toBeTruthy();
+  });
+
+  test('Response should return a error message', async () => {
+    const response = await request(app).get('/api/super-admins/:id').send();
+    expect(response.message).not.toBeNull();
+  });
+
+  test('Response should return a 404 status', async () => {
+    const response = await request(app).get('/api/super-admins/6283f334fcd4fe8e83f960d3').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('Response should return true error', async () => {
+    const response = await request(app).get('/api/super-admins/6283f334fcd4fe8e83f960d2').send();
+    expect(response.error).toBeTruthy();
+  });
+
+  test('Response should return a error message', async () => {
+    const response = await request(app).get('/api/super-admins/6283f334fcd4fe8e83f960d1').send();
+    expect(response.message).not.toBeNull();
+  });
+});
