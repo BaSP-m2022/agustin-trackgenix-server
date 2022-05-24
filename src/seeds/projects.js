48411cf8d9866ef2ae0754464bf42aa8430f629c
Cristian Alcober <cristian_alcober@outlook.com>
2022-05-23 22:28:47 -0300

    TG-14: Testing endpoints of POST and DELETE

--- src/seeds/projects.js
+++ src/seeds/projects.js
@@ -2,20 +2,20 @@ import mongoose from 'mongoose';
 
 export default [{
   rates: {
-      dev: 81,
-      pm: 55,
-      qa: 81
+    dev: 81,
+    pm: 55,
+    qa: 81,
   },
-  _id: "628281797161c78df5f6ac9e",
-  name: "Rochell Hattersley",
-  description: "Division of Acoustic Nerve, Open Approach",
-  status: "false",
-  client: "Kambaa",
-  employees: 
+  _id: '628281797161c78df5f6ac9e',
+  name: 'Rochell Hattersley',
+  description: 'Division of Acoustic Nerve, Open Approach',
+  status: 'false',
+  client: 'Kambaa',
+  employees:
       {
-          name: "Gwenneth",
-          lastName: "Pawling",
-          role: "DEV",
-          _id: "628281797161c78df5f6ac9f"
-      }
+        name: 'Gwenneth',
+        lastName: 'Pawling',
+        role: 'DEV',
+        _id: '628281797161c78df5f6ac9f',
+      },
 }];
