12d358c9fea796d67473571c6c1cc0589cba9e31
Guido Cerioni <guidocerioni98@gmail.com>
2022-05-24 23:54:48 -0300

    TG-15: add put and delete test cases

--- src/tests/time-sheets.test.js
+++ src/tests/time-sheets.test.js
@@ -130,3 +130,151 @@ describe('GET BY ID /time-sheets/:id', () => {
     },
   );
 });
+
+let idEmployeeCreated;
+
+const requestBody = {
+  date: '2021-02-05T03:00:00.000Z',
+  regularHours: 1,
+  overtimeHours: 6,
+  startTime: 10,
+  endTime: 11,
+  task: '62824ff02de0708e369fc99c',
+  employee: '62825273c598dd23e9742740',
+  project: '628281797161c78df5f6ac9e',
+};
+const incorrectRequestBody = {
+  date: '2021-02-05T03:00:00.000Z',
+  regularHours: 'aA',
+  overtimeHours: 6,
+  startTime: 10,
+  endTime: 11,
+  task: '62824ff02de0708e369fc99c',
+  employee: '62825273c598dd23e9742740',
+  project: '628281797161c78df5f6ac9e',
+};
+
+describe('POST /time-sheets', () => {
+  test('With a correct timesheet body the response should return a status 201', async () => {
+    const response = await request(app).post('/api/time-sheets').send(requestBody);
+    expect(response.status).toBe(201);
+    // eslint-disable-next-line no-underscore-dangle
+    idEmployeeCreated = response.body.data._id;
+  });
+
+  test('With a correct timesheet body the response should return a correct message', async () => {
+    const response = await request(app).post('/api/time-sheets').send(requestBody);
+    expect(response.body.message).toEqual('The time-sheet created successfully');
+  });
+
+  test('With a correct timesheet body the response should return the created timesheet data', async () => {
+    const response = await request(app).post('/api/time-sheets').send(requestBody);
+    expect(response.body.data).toMatchObject(requestBody);
+  });
+
+  test('With a correct timesheet body the response should return a error false', async () => {
+    const response = await request(app).post('/api/time-sheets').send(requestBody);
+    expect(response.body.error).toBe(false);
+  });
+
+  test('With an incorrect timesheet body the response should return a status 400', async () => {
+    const response = await request(app).post('/api/time-sheets').send(incorrectRequestBody);
+    expect(response.status).toBe(400);
+  });
+
+  test('With an incorrect timesheet body the response should return error info in data', async () => {
+    const response = await request(app).post('/api/time-sheets').send(incorrectRequestBody);
+    expect(response.body.data).toMatch(/^[a-zA-Z0-9_ /'"]/);
+  });
+
+  test('With an incorrect timesheet body the response should return error true', async () => {
+    const response = await request(app).post('/api/time-sheets').send(incorrectRequestBody);
+    expect(response.body.error).toBeTruthy();
+  });
+});
+
+/*
+const putTimeSheetCases = [
+  ['date', '2021-04-05T03:00:00.000Z'],
+  ['regularHours', 44],
+  ['overtimeHours', 44],
+  ['startTime', 44],
+  ['endTime', 44],
+  ['task', '62824ff02de0708e369fc99c'],
+  ['employee', '62825273c598dd23e9742740'],
+  ['project', '628281797161c78df5f6ac9e'],
+];
+*/
+
+const putRequestBody = { date: '2021-04-05T03:00:00.000Z' };
+const incorrectPutRequestBody = { regularHours: 'asdasd' };
+describe('PUT /time-sheets', () => {
+  test('With a correct timesheet body the response should return a status 200', async () => {
+    const response = await request(app).put('/api/time-sheets/62817f32ba9843827a61371f').send(putRequestBody);
+    expect(response.status).toBe(200);
+    // eslint-disable-next-line no-underscore-dangle
+    idEmployeeCreated = response.body.data._id;
+  });
+
+  test('With a correct timesheet body the response should return a correct message', async () => {
+    const response = await request(app).put('/api/time-sheets/62817f32ba9843827a61371f').send(putRequestBody);
+    expect(response.body.message).toEqual('The time-sheet updated successfully');
+  });
+
+  test('With a correct timesheet body the response should return a error false', async () => {
+    const response = await request(app).put('/api/time-sheets/62817f32ba9843827a61371f').send(putRequestBody);
+    expect(response.body.error).toBe(false);
+  });
+
+  test('With an incorrect timesheet body the response should return a status 400', async () => {
+    const response = await request(app).put('/api/time-sheets/62817f32ba9843827a61371f').send(incorrectPutRequestBody);
+    expect(response.status).toBe(400);
+  });
+
+  test('With an incorrect timesheet body the response should return error info in data', async () => {
+    const response = await request(app).put('/api/time-sheets/62817f32ba9843827a61371f').send(incorrectPutRequestBody);
+    expect(response.body.data).toMatch(/^[a-zA-Z0-9_ /'"]/);
+  });
+
+  test('With an incorrect timesheet body the response should return error true', async () => {
+    const response = await request(app).put('/api/time-sheets/62817f32ba9843827a61371f').send(incorrectPutRequestBody);
+    expect(response.body.error).toBeTruthy();
+  });
+});
+
+describe('DELETE /time-sheets/:id', () => {
+  test('With an incorrect id the response should return a status 400', async () => {
+    const response = await request(app).delete('/api/time-sheets/asdf56165').send();
+    expect(response.status).toBe(400);
+  });
+
+  test('With an incorrect route the response should return a status 404', async () => {
+    const response = await request(app).delete('/api/employe/62817f32ba9843827a61371f').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('With an nonexistent id the response should return a status 404', async () => {
+    const response = await request(app).delete('/api/time-sheets/62898d14882f8759987f5b50').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('With an nonexistent id the response should return a error true', async () => {
+    const response = await request(app).delete('/api/time-sheets/62898d14882f8759987f5b50').send();
+    expect(response.body.error).toBe(true);
+  });
+
+  test('With an nonexistent id the response should return a data undefined', async () => {
+    const response = await request(app).delete('/api/time-sheets/62898d14882f8759987f5b50').send();
+    expect(response.body.data).toBeUndefined();
+  });
+
+  test('With an nonexistent id the response should return a non empty message', async () => {
+    const response = await request(app).delete('/api/time-sheets/62898d14882f8759987f5b50').send();
+    expect(response.body.message.length).toBeGreaterThan(5);
+  });
+
+  test('With an correct id the response should return a status 204', async () => {
+    const response = await request(app).delete(`/api/time-sheets/${idEmployeeCreated}`).send();
+    expect(response.status).toBe(204);
+  });
+});
